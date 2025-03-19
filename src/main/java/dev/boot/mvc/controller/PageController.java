c2ee979bdca4f6c27a800dc7f142a618cf8d354c
xksdn <xksdn7623@gmail.com>
2025-03-19 18:19:48 +0900

    add category page

--- src/main/java/dev/boot/mvc/controller/PageController.java
+++ src/main/java/dev/boot/mvc/controller/PageController.java
@@ -1,15 +1,25 @@
 package dev.boot.mvc.controller;
 
+import dev.boot.mvc.db.CategoryVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/cate")
 public class PageController {
 
   @GetMapping("/")
   public String page() {
     return "index";
+  }
+
+  @GetMapping("/create")
+  public String create(@ModelAttribute("categoryVO")CategoryVO categoryVO) {
+    System.out.println("-> http://localhost:9092/cate/create");
+    return "/cate/create";
   }
 }
