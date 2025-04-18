f6fba2cea3379c61c675ed28565191baf061b965
xksdn <xksdn7623@gmail.com>
2025-04-18 18:06:20 +0900

    fix record_per_page

--- src/main/java/dev/boot/mvc/controller/PageController.java
+++ src/main/java/dev/boot/mvc/controller/PageController.java
@@ -34,7 +34,7 @@ public class PageController {
   private UserProcinter userProcinter;
 
   /** 페이지당 출력할 레코드 갯수, nowPage는 1부터 시작 */
-  public int record_per_page = 4;
+  public int record_per_page = 6;
 
   /** 블럭당 페이지 수, 하나의 블럭은 10개의 페이지로 구성됨 */
   public int page_per_block = 10;
